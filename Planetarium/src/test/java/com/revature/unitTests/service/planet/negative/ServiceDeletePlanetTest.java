3fc1966871d918cde24271ccb0df186945ea9abb
ethbra-revature <184133318+ethbra-revature@users.noreply.github.com>
2025-01-13 15:46:11 -0600

    Added Select negative tests

--- Planetarium/src/test/java/com/revature/unitTests/service/planet/negative/ServiceDeletePlanetTest.java
+++ Planetarium/src/test/java/com/revature/unitTests/service/planet/negative/ServiceDeletePlanetTest.java
@@ -1,9 +1,15 @@
 package com.revature.unitTests.service.planet.negative;
 
+import com.revature.unitTests.service.planet.ServicePlanetUtil;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-public class ServiceDeletePlanetTest {
+
+@RunWith(Parameterized.class)
+public class ServiceDeletePlanetTest extends ServicePlanetUtil {
     @Test
     public void serviceDeletePlanetNegatives() {
+
     }
 }
--- Planetarium/src/test/java/com/revature/unitTests/service/planet/negative/ServiceSelectPlanetTest.java
+++ Planetarium/src/test/java/com/revature/unitTests/service/planet/negative/ServiceSelectPlanetTest.java
@@ -1,10 +1,44 @@
 package com.revature.unitTests.service.planet.negative;
 
+import com.revature.planetarium.entities.Planet;
+import com.revature.unitTests.service.planet.ServicePlanetUtil;
+import com.revature.util.TestDataLoader;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+import org.mockito.Mockito;
 
-public class ServiceSelectPlanetTest {
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class ServiceSelectPlanetTest extends ServicePlanetUtil {
+
+    @Parameter
+    public int ownerId;
+
+
+
+    @Parameters
+    public static Collection<Object[]> data() throws FileNotFoundException {
+        return Arrays.asList(new Object[][]{
+                {-1000},
+                {9999}
+        });
+    }
+
     @Test
     public void serviceSelectPlanetNegatives() {
+
+        Mockito.when(dao.readPlanetsByOwner(Mockito.any())).thenReturn(List.of());
+
+        assertEquals(List.of(), service.selectByOwner(ownerId));
 
     }
 }
