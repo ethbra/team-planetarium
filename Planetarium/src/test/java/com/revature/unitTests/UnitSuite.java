b8446746df83273d7b219bd84155f2e81f250005
alexylv <alex.yliao71@gmail.com>
2025-01-14 12:45:50 -0500

    planet service negative test for deletion

--- Planetarium/src/test/java/com/revature/unitTests/UnitSuite.java
+++ Planetarium/src/test/java/com/revature/unitTests/UnitSuite.java
@@ -1,6 +1,7 @@
 package com.revature.unitTests;
 
 import com.revature.unitTests.service.planet.negative.ServiceCreatePlanetTest;
+import com.revature.unitTests.service.planet.negative.ServiceDeletePlanetTest;
 import com.revature.unitTests.service.planet.positive.ServicePlanetTest;
 import com.revature.unitTests.service.user.positive.ServiceUserTest;
 import org.junit.runner.RunWith;
@@ -8,9 +9,8 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        ServiceCreatePlanetTest.class
+        ServiceDeletePlanetTest.class
 })
 public class UnitSuite {
-
 
 }
